967130c418a0f89843d0ca8f43cee1b6e2932ff6
MrMohone <mohammedawol0837@gmail.com>
2025-06-20 01:44:09 +0300

    used model included

---
+++ blogapp/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+   bio = models.TextField(blank=True, null=True)
+   profile_picture = models.ImageField(upload_to='profile_img/', blank=True, null=True)
+   facebook = models.URLField(max_length=225, blank=True, null=True)
+   youtube = models.URLField(max_length=225, blank=True, null=True)
+   instagram = models.URLField(max_length=225, blank=True, null=True)
+   tiwtter = models.URLField(max_length=225, blank=True, null=True)
+   
+   def __str__(self):
+       return self.username
+   
