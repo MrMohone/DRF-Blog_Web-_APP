e9866e440f43af379e45c95bf38fc3ef04ac79e7
MrMohone <mohammedawol0837@gmail.com>
2025-06-23 23:37:13 +0300

    model for 'get_user_model' or user is  serialized

--- new_django_api/settings.py
+++ new_django_api/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blogapp'
+    'blogapp',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -121,4 +122,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 AUTH_USER_MODEL = 'blogapp.CustomUser'
