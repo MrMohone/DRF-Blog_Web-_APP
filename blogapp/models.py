219f55f9d45a5f9ac58d98c3b71693faefa9a04c
MrMohone <mohammedawol0837@gmail.com>
2025-06-24 02:06:06 +0300

    simple jwt are commited

--- blogapp/models.py
+++ blogapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 
 class CustomUser(AbstractUser):
    bio = models.TextField(blank=True, null=True)
@@ -11,4 +12,6 @@ class CustomUser(AbstractUser):
    
    def __str__(self):
        return self.username
+   
+
    
--- new_django_api/settings.py
+++ new_django_api/settings.py
@@ -124,3 +124,11 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'blogapp.CustomUser'
+
+# REST Framework settings
+# Using Simple JWT for authentication
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
