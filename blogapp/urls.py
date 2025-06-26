3197c25afda5f7231f793dcb79bd654bc16dedf8
MrMohone <mohammedawol0837@gmail.com>
2025-06-26 23:57:40 +0300

    put method commit

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('register_user/', views.register_user, name='register_user'),
     path('create_blog/', views.create_blog, name='create_blog'),
+    path('blog_list', views.blog_list, name='blog_list'),
+    path('update_blog/<int:pk>/', views.update_blog, name='update_blog'),
 ]
--- blogapp/views.py
+++ blogapp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Blog
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
@@ -21,5 +22,24 @@ def create_blog(request):
     serializer = BlogSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save(author=user)
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+def blog_list(request):
+    blogs = Blog.objects.all()
+    serializer = BlogSerializer(blogs, many=True)
+    return Response(serializer.data)
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated]) 
+def update_blog(request, pk):
+    user = request.user
+    blog = Blog.objects.get(id=pk)
+    if blog.author != user:
+        return Response({'error':'You are not author of this blog'},status=status.HTTP_403_FORBIDDEN)
+    serializer = BlogSerializer(blog, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
         return Response(serializer.data)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
